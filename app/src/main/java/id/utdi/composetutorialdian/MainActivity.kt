c335688ae2387104dd9b8ac22a0ece934e3216a2
yandiaan <diansetiawan2121@gmail.com>
2023-09-15 12:57:45 +0700

    fix

--- app/src/main/java/id/utdi/composetutorialdian/MainActivity.kt
+++ app/src/main/java/id/utdi/composetutorialdian/MainActivity.kt
@@ -1,33 +1,91 @@
 package id.utdi.composetutorialdian
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+ import androidx.compose.material3.MaterialTheme
+ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-//import id.utdi.composetutorialdian.ui.theme.ComposeTutorialDianTheme
+import androidx.compose.ui.unit.dp
+import id.utdi.composetutorialdian.ui.theme.ComposeTutorialDianTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Android")
+            ComposeTutorialDianTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    GreetCard(Greeting("Dian", "Hallo, Nama saya dian"))
+                }
+            }
         }
     }
 }
 
+data class Greeting(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun GreetCard(msg: Greeting) {
+    Row (modifier = Modifier.padding(all = 8.dp)) {
+       Image(
+           painter = painterResource(R.drawable.dian),
+           contentDescription = "Foto Dian Setiawan",
+           modifier = Modifier
+               .size(40.dp)
+               .clip(CircleShape)
+               .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+       )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleMedium,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+    ComposeTutorialDianTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            GreetCard(Greeting("Dian", "Hallo, Nama saya dian"))
+        }
+    }
 }
