980e4195e49cc2669067ced42b48d5128225a8c5
yandiaan <diansetiawan2121@gmail.com>
2023-09-15 10:59:21 +0700

    Initial commit

---
+++ app/src/main/java/id/utdi/composetutorialdian/MainActivity.kt
@@ -0,0 +1,33 @@
+package id.utdi.composetutorialdian
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+//import id.utdi.composetutorialdian.ui.theme.ComposeTutorialDianTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
+}
